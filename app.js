22c6ef0803af46db83848643d50de27648ad05ea
Cesar Nobre <cesarnobrefilho@gmail.com>
2016-03-13 20:57:00 -0300

    added implementation jade js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+var jade = require('jade');
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '../public'));
+
 app.use(express.static('views'));
 app.use(express.static('public'));
-
+app.set('view engine', 'jade');
 app.get('/', function (req, res) {
 
+});
+
+app.get('/arroz', function (req, res) {
+	res.render('views/index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(3000, function () {});
