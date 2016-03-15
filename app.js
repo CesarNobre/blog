cc9e9b6e78c7b2fce624786e845b776f085a1830
cesarnobre <cesarnobrefilho@gmail.com>
2016-03-15 07:44:13 -0300

    parsing to jade js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var jade = require('jade');
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.use(express.static(__dirname + '../public'));
-
-app.use(express.static('views'));
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.get('/', function (req, res) {
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+	res.render('views/index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.get('/arroz', function (req, res) {
